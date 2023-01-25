a05ba49b95c2e3868abc9a47029445d4350297a4
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2023-01-25 13:47:48 +0600

    SA-33 - added signIn component, styles and auth slices and actions

--- src/containers/SignIn/SignIn.tsx
+++ src/containers/SignIn/SignIn.tsx
@@ -1,62 +1,127 @@
-import { Button, Checkbox, Col, Form, Row } from 'antd';
+import { Button, Col, Form, Row, Typography } from 'antd';
 import bem from 'easy-bem';
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import image from 'assets/images/tracktor.jpeg';
+import logo from 'assets/images/logo.png';
+import tractor from 'assets/images/tracktor.png';
 import FormField from 'components/FormField/FormField';
+import ModalPasswordReset from 'components/ModalComponent/ModalChildrenComponents/ModalPasswordReset/ModalPasswordReset';
+import ModalComponent from 'components/ModalComponent/ModalComponent';
+import { authSelector } from 'redux/auth/authSlice';
 import 'containers/SignIn/_signIn.scss';
+
+const { Title } = Typography;
 
 const SignIn: React.FC = () => {
   const b = bem('SignIn');
+  const dispatch = useDispatch();
+  const { loading, commonError, user, success } = useSelector(authSelector);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const onFinish = () => {};
+
   return (
-    <Row className={b()} justify='space-between' data-testid='sign-in'>
-      <Col
-        className={b('img-position')}
-        xs={{ span: 6, offset: 0 }}
-        lg={{ span: 6, offset: 0 }}
-        md={{ span: 6, offset: 0 }}
-      >
-        <img className={b('img-styles')} src={image} alt='image2' />
-      </Col>
-      <Col xs={{ span: 12, offset: 6 }} lg={{ span: 6, offset: 9 }} md={{ span: 12, offset: 6 }}>
-        <Form
-          className={b('form-styles')}
-          layout='vertical'
-          name='basic'
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete='off'
+    <>
+      <Row justify='space-between' data-testid='sign-in' className={b()}>
+        <Col xs={{ span: 24, offset: 0 }} md={{ span: 24, offset: 0 }} lg={{ span: 12, offset: 0 }}>
+          <img src={tractor} alt='tractor' className={b('image-styles')} />
+        </Col>
+        <Col
+          className={b('main-colum-styles')}
+          xs={{ span: 13, offset: 5 }}
+          md={{ span: 13, offset: 5 }}
+          lg={{ span: 7, offset: 1 }}
         >
-          <FormField
-            data-testid='email_id_login'
-            className='username'
-            type='email'
-            id='email_id'
-            label='Логин'
-            name='email'
-            placeholder='Введите логин'
-          />
-          <FormField
-            data-testid='password_id'
-            className='username'
-            type='password'
-            label='Пароль'
-            name='password'
-            placeholder='Введите пароль'
-          />
-          <Form.Item name='remember' valuePropName='checked' wrapperCol={{ offset: 17 }}>
-            <Checkbox className={b('checkbox-styles')}>Запомнить</Checkbox>
-          </Form.Item>
-          <Form.Item wrapperCol={{ offset: 9 }}>
-            <Button type='primary' htmlType='submit'>
-              Войти
+          <img src={logo} alt='logo' />
+          <p className={b('subtitle')}>Добро пожаловать</p>
+          <Title data-testid='sign_in_test' className={b('title')}>
+            Вход в аккаунт
+          </Title>
+
+          <Form
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            className='login-form'
+            autoComplete='off'
+            layout='vertical'
+          >
+            <FormField
+              data-testid='email_id_login'
+              type='email'
+              id='email_id'
+              inputClassName={b('username')}
+              label='Логин'
+              name='email'
+              placeholder='Логин'
+            />
+
+            <FormField
+              data-testid='password_id'
+              className='username'
+              type='password'
+              label='Пароль'
+              name='remember'
+              placeholder='Пароль'
+            />
+
+            <FormField
+              type='checkbox'
+              name='remember'
+              label='Запомнить меня'
+              valuePropName='checked'
+            />
+
+            <Button
+              disabled={!!commonError}
+              type='primary'
+              loading={!!loading}
+              style={{ width: '100%', borderRadius: 4 }}
+              className={b('login-form-button')}
+            >
+              Продолжить
             </Button>
-          </Form.Item>
-        </Form>
-      </Col>
-    </Row>
+          </Form>
+          <Row
+            gutter={{
+              xs: 0,
+              sm: 0,
+              md: 0,
+              lg: 0,
+            }}
+            className={b('buttons-col')}
+          >
+            <Button type='link'>
+              <Link to={'/'}>Зарегистрироваться</Link>
+            </Button>
+            <Button type='link' onClick={showModal}>
+              Восстановить пароль
+            </Button>
+          </Row>
+        </Col>
+      </Row>
+      <ModalComponent
+        title='Восстановление пароля'
+        open={isModalOpen}
+        handleOk={handleOk}
+        handleCancel={handleCancel}
+      >
+        <ModalPasswordReset />
+      </ModalComponent>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from 'App';
 import { store } from 'redux/store';
-import 'assets/scss/index.scss';
+import 'assets/scss/_index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
