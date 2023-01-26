c2d85625d844c780c8f4c389cf2c4ee0201e6bde
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2023-01-26 14:34:10 +0600

    SA-34 - added route for registration component - updated jest npm and settings for test - updated tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 
 import AppRouter from 'AppRouter/AppRouter';
+import Registration from 'containers/Registration/Registration';
 import SignIn from 'containers/SignIn/SignIn';
 import { authSelector } from 'redux/auth/authSlice';
 import { useAppSelector } from 'redux/hooks';
@@ -15,6 +16,7 @@ const App: React.FC = () => {
         <AppRouter />
       ) : (
         <Routes>
+          <Route path='/sign-up/' element={<Registration />} />
           {success && user && tokens ? null : <Route path='*' element={<SignIn />} />}
         </Routes>
       )}
