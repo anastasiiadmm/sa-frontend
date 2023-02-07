b30b11bd0ddfffefbc243da6c1b30092a8af59bf
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2023-02-07 12:14:51 +0600

    SA-75 - added create new user form - added styles and icons - added card to users table

--- src/containers/Users/NewUser/NewUser.tsx
+++ src/containers/Users/NewUser/NewUser.tsx
@@ -1,7 +1,149 @@
+import { Button, Col, Form, Typography } from 'antd';
+import bem from 'easy-bem';
 import React from 'react';
 
+import FormField from 'components/FormField/FormField';
+import 'containers/Users/NewUser/_NewUser.scss';
+
+const { Title } = Typography;
+
 const NewUser: React.FC = () => {
-  return <div>NewUser</div>;
+  const b = bem('NewUser');
+  const [form] = Form.useForm();
+
+  const onFinish = (values: any) => {};
+
+  return (
+    <div className='layout'>
+      <Col
+        className={b('')}
+        xs={{ span: 13, offset: 5 }}
+        md={{ span: 9, offset: 5 }}
+        lg={{ span: 11, offset: 1 }}
+      >
+        <Title level={3} data-testid='sign_in_test'>
+          Добавить нового пользователя
+        </Title>
+        <p className={b('subtext')}>Создать учетную запись нового пользователя и добавить его.</p>
+
+        <Form
+          form={form}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete='off'
+          layout='vertical'
+        >
+          <FormField
+            data-testid='username_id'
+            id='username_id'
+            inputClassName={b('username')}
+            label='Username'
+            name='username'
+            placeholder='Username'
+          />
+
+          <div className={b('form-block')}>
+            <FormField
+              id='password_id'
+              type='password'
+              className='username'
+              name='password'
+              label='Пароль'
+              placeholder='Пароль'
+            />
+
+            <FormField
+              id='password_confirm'
+              type='password'
+              className='username'
+              name='confirm_password'
+              dependencies={['password']}
+              label='Повторите пароль'
+              placeholder='Повторите пароль'
+            />
+          </div>
+
+          <div className={b('form-block')}>
+            <FormField
+              data-testid='first_name_id'
+              id='first_name_id'
+              inputClassName={b('username')}
+              label='Фамилия'
+              name='first_name'
+              placeholder='Фамилия'
+            />
+
+            <FormField
+              data-testid='last_name_id'
+              id='last_name_id'
+              inputClassName={b('username')}
+              label='Имя'
+              name='last_name'
+              placeholder='Имя'
+            />
+          </div>
+
+          <FormField
+            data-testid='surname_id'
+            id='surname_id'
+            inputClassName={b('username')}
+            label='Отчество'
+            name='surname'
+            placeholder='Отчество'
+          />
+
+          <div className={b('form-block')}>
+            <FormField
+              data-testid='email_id_login'
+              type='email'
+              id='email_id'
+              inputClassName={b('username')}
+              label='Email'
+              name='email'
+              placeholder='Email'
+            />
+
+            <FormField
+              type='phone'
+              className='username'
+              name='phone'
+              label='Номер телефона'
+              placeholder='Номер телефона'
+            />
+          </div>
+
+          <FormField
+            data-testid='name_of_farm_id'
+            id='name_of_farm_id'
+            inputClassName={b('username')}
+            label='Название колхоза/фермы/компании'
+            name='name_of_farm'
+            placeholder='Название колхоза/фермы/компании'
+          />
+
+          <FormField
+            data-testid='region_id'
+            id='region_id'
+            inputClassName={b('username')}
+            label='Регион расположения'
+            name='region'
+            placeholder='Регион расположения'
+          />
+
+          <Button
+            // disabled={!!commonError}
+            type='primary'
+            htmlType='submit'
+            // loading={!!loading}
+            style={{ width: '100%', borderRadius: 4 }}
+            className={b('login-form-button')}
+          >
+            Добавить нового пользователя
+          </Button>
+        </Form>
+      </Col>
+    </div>
+  );
 };
 
 export default NewUser;
