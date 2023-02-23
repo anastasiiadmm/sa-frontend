aa37730e04cde89684f14c9be0184377bb3d5308
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2023-02-23 15:06:01 +0600

    SA-125 - added request register user modal - added styles, updated tests - added template for succeed register user modal info

---
+++ src/components/ModalComponent/ModalChildrenComponents/RequestRegisterModal/RequestRegisterModal.tsx
@@ -0,0 +1,145 @@
+import { Button, Col, Form } from 'antd';
+import bem from 'easy-bem';
+import React from 'react';
+
+import FormField from 'components/FormField/FormField';
+import 'components/ModalComponent/ModalChildrenComponents/RequestRegisterModal/_requestRegisterModal.scss';
+
+const RequestRegisterModal = () => {
+  const b = bem('RequestRegisterModal');
+  const [form] = Form.useForm();
+
+  const onFinish = (values: any) => {};
+
+  /*  const successSend = (
+    <>
+      <AlertComponent
+        message='Спасибо за заявку. Ваш запрос был отправлен модератору'
+        type='info'
+        showIcon
+      />
+      <Button
+        className={b('close-form-button')}
+        type='primary'
+        style={{ width: '100%', borderRadius: 4 }}
+      >
+        Закрыть
+      </Button>
+    </>
+  ); */
+
+  return (
+    <Col
+      className={b('')}
+      xs={{ span: 20, offset: 2 }}
+      md={{ span: 22, offset: 1 }}
+      lg={{ span: 22, offset: 1 }}
+    >
+      <Form
+        form={form}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete='off'
+        layout='vertical'
+      >
+        <div className={b('form-block')}>
+          <FormField
+            bordered
+            data-testid='first_name_id'
+            id='first_name_id'
+            inputClassName={b('username')}
+            label='Фамилия'
+            name='first_name'
+            placeholder='Фамилия'
+          />
+
+          <FormField
+            bordered
+            data-testid='last_name_id'
+            id='last_name_id'
+            inputClassName={b('username')}
+            label='Имя'
+            name='last_name'
+            placeholder='Имя'
+          />
+        </div>
+
+        <FormField
+          bordered
+          data-testid='surname_id'
+          id='surname_id'
+          inputClassName={b('username')}
+          label='Отчество'
+          name='surname'
+          placeholder='Отчество'
+        />
+
+        <div className={b('form-block')}>
+          <FormField
+            bordered
+            data-testid='email_id_login'
+            type='email'
+            id='email_id'
+            inputClassName={b('username')}
+            label='Email'
+            name='email'
+            placeholder='Email'
+          />
+
+          <FormField
+            bordered
+            type='phone'
+            className='username'
+            name='phone'
+            label='Номер телефона'
+            placeholder='Номер телефона'
+          />
+        </div>
+
+        <FormField
+          bordered
+          data-testid='name_of_farm_id'
+          id='name_of_farm_id'
+          inputClassName={b('username')}
+          label='Название колхоза/фермы/компании'
+          name='name_of_farm'
+          placeholder='Название колхоза/фермы/компании'
+        />
+
+        <FormField
+          bordered
+          data-testid='region_id'
+          id='region_id'
+          inputClassName={b('username')}
+          label='Регион расположения'
+          name='region'
+          placeholder='Регион расположения'
+        />
+
+        <div className={b('form-modal-buttons')}>
+          <Button
+            type='primary'
+            style={{ width: '100%', borderRadius: 4 }}
+            className={b('delete-profile-button')}
+            // onClick={deleteUserHandler}
+          >
+            Отмена
+          </Button>
+
+          <Button
+            // disabled={!!commonError}
+            type='primary'
+            htmlType='submit'
+            // loading={!!loading}
+            style={{ width: '100%', borderRadius: 4 }}
+            className={b('login-form-button')}
+          >
+            Отправить
+          </Button>
+        </div>
+      </Form>
+    </Col>
+  );
+};
+
+export default RequestRegisterModal;
--- src/containers/SignIn/SignIn.tsx
+++ src/containers/SignIn/SignIn.tsx
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router';
 import logo from 'assets/images/logo.png';
 import tractor from 'assets/images/tracktor.png';
 import FormField from 'components/FormField/FormField';
+import RequestRegisterModal from 'components/ModalComponent/ModalChildrenComponents/RequestRegisterModal/RequestRegisterModal';
+import ModalComponent from 'components/ModalComponent/ModalComponent';
 import { authSelector, loginUser } from 'redux/auth/authSlice';
 import { useAppDispatch, useAppSelector } from 'redux/hooks';
 import { LoginMutation } from 'types';
@@ -22,6 +24,19 @@ const SignIn: React.FC = () => {
   const { success, loading, user } = useAppSelector(authSelector);
   const history = useNavigate();
   const [checked, setChecked] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const onFinish = async (values: LoginMutation) => {
     try {
@@ -46,71 +61,95 @@ const SignIn: React.FC = () => {
   }, [user, success]);
 
   return (
-    <Row justify='space-between' data-testid='sign-in' className={b()}>
-      <Col xs={{ span: 24, offset: 0 }} md={{ span: 24, offset: 0 }} lg={{ span: 12, offset: 0 }}>
-        <img src={tractor} alt='tractor' className={b('image-styles')} />
-      </Col>
-      <Col
-        className={b('main-colum-styles')}
-        xs={{ span: 13, offset: 5 }}
-        md={{ span: 13, offset: 5 }}
-        lg={{ span: 7, offset: 1 }}
-      >
-        <img src={logo} alt='logo' />
-        <p className={b('subtitle')}>Добро пожаловать</p>
-        <Title data-testid='sign_in_test' className={b('title')}>
-          Вход в аккаунт
-        </Title>
-
-        <Form
-          form={form}
-          name='register'
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          className='login-form'
-          autoComplete='off'
-          layout='vertical'
+    <>
+      <Row justify='space-between' data-testid='sign-in' className={b()}>
+        <Col xs={{ span: 24, offset: 0 }} md={{ span: 24, offset: 0 }} lg={{ span: 12, offset: 0 }}>
+          <img src={tractor} alt='tractor' className={b('image-styles')} />
+        </Col>
+        <Col
+          className={b('main-colum-styles')}
+          xs={{ span: 13, offset: 5 }}
+          md={{ span: 13, offset: 5 }}
+          lg={{ span: 7, offset: 1 }}
         >
-          <FormField
-            bordered
-            data-testid='email_id_login'
-            type='email'
-            id='email_id'
-            label='Логин'
-            name='username'
-            placeholder='Логин'
-          />
+          <img src={logo} alt='logo' />
+          <p className={b('subtitle')}>Добро пожаловать</p>
+          <Title data-testid='sign_in_test' className={b('title')}>
+            Вход в аккаунт
+          </Title>
 
-          <FormField
-            bordered
-            data-testid='password_id'
-            type='password'
-            label='Пароль'
-            name='password'
-            placeholder='Пароль'
-          />
-
-          <FormField
-            id='remember_id'
-            type='checkbox'
-            label='Запомнить меня'
-            valuePropName='password'
-            checked={checked}
-            onChange={onChangeCheckbox}
-          />
-
-          <Button
-            type='primary'
-            htmlType='submit'
-            loading={!!loading}
-            style={{ width: '100%', borderRadius: 4 }}
-            className={b('login-form-button')}
+          <Form
+            form={form}
+            name='register'
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            className='login-form'
+            autoComplete='off'
+            layout='vertical'
           >
-            Продолжить
-          </Button>
-        </Form>
-      </Col>
-    </Row>
+            <FormField
+              bordered
+              data-testid='email_id_login'
+              type='email'
+              id='email_id'
+              label='Логин'
+              name='username'
+              placeholder='Логин'
+            />
+
+            <FormField
+              bordered
+              data-testid='password_id'
+              type='password'
+              label='Пароль'
+              name='password'
+              placeholder='Пароль'
+            />
+
+            <FormField
+              id='remember_id'
+              type='checkbox'
+              label='Запомнить меня'
+              valuePropName='password'
+              checked={checked}
+              onChange={onChangeCheckbox}
+            />
+
+            <Button
+              type='primary'
+              htmlType='submit'
+              loading={!!loading}
+              style={{ width: '100%', borderRadius: 4 }}
+              className={b('login-form-button')}
+            >
+              Продолжить
+            </Button>
+          </Form>
+          <Row
+            gutter={{
+              xs: 0,
+              sm: 0,
+              md: 0,
+              lg: 0,
+            }}
+            className={b('buttons-col')}
+          >
+            <Button type='link' onClick={showModal}>
+              Хотите стать клиентом? Зарегистрироваться
+            </Button>
+          </Row>
+        </Col>
+      </Row>
+
+      <ModalComponent
+        title='Отправить запрос на регистрацию'
+        open={isModalOpen}
+        handleOk={handleOk}
+        handleCancel={handleCancel}
+      >
+        <RequestRegisterModal />
+      </ModalComponent>
+    </>
   );
 };
 
