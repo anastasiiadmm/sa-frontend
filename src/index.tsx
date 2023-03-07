3b3fff4baefd0beeea45dcdd0def31b0e9f3b4d7
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2023-03-07 17:47:38 +0600

    SA-149 - fixed eslint error

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
+import { ConfigProvider } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
-import { ConfigProvider } from 'antd';
 
 /* eslint-disable */
 import store from 'redux/store';
