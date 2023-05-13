67f37bf8b310e6cd96a5098bc34572665d825d3b
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2023-05-13 17:04:18 +0600

    SA-261 - fixed approve request registration

--- src/components/ModalComponent/ModalChildrenComponents/RequestsModals/RequestRegisterUser/RequestRegisterUser.tsx
+++ src/components/ModalComponent/ModalChildrenComponents/RequestsModals/RequestRegisterUser/RequestRegisterUser.tsx
@@ -142,7 +142,7 @@ const RequestRegisterUser: React.FC<Props> = ({
       await dispatch(
         accountManagerConfirmationRequest({
           id: userId,
-          data: isEmptyObject(data) ? userData : data,
+          data: isEmptyObject(data) ? undefined : data,
         }),
       ).unwrap();
       showAgreeModal();
