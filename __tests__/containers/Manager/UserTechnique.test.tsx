e93a3be8a157472bb42391df7667e7e177c6d406
atomzcoffee <erjan.azimov@gmail.com>
2023-06-20 09:56:02 +0600

    SA-510-Tablet-layout-and-mobile-version-of-technology-pages

---
+++ __tests__/containers/Manager/UserTechnique.test.tsx
@@ -0,0 +1,41 @@
+import { BrowserRouter } from "react-router-dom";
+import { cleanup } from "@testing-library/react";
+import "../../../__mocks__/matchMedia.mock";
+import "@testing-library/jest-dom";
+import { mockedDispatch, mockedUseSelectors } from "../../../__mocks__/utils";
+
+import UserTechnique from "../../../src/containers/Manager/Users/UserTechnique/UserTechnique";
+import renderer from "react-test-renderer";
+
+afterEach(cleanup);
+
+const data = {
+    vehicleList: [
+        {
+            "id": 91,
+            "code": "D8BN8TR61",
+            "description": "fbd",
+            "jobs_number": 1,
+            "area": "927.006",
+            "image": "/api/static/default_vehicle_image.jpg"
+        }
+    ]
+};
+
+
+describe("<UserTechnique />", () => {
+    test("Users table component should be in the document", async () => {
+        mockedUseSelectors.mockReturnValue(data);
+        const dispatch = jest.fn();
+        mockedDispatch.mockReturnValue(dispatch);
+
+        const tree = renderer
+            .create(
+                <BrowserRouter>
+                    <UserTechnique />
+                </BrowserRouter>,
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
