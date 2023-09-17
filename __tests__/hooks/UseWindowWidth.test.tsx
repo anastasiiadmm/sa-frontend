473fc1691afd46ec318aae21c725fe108dcf343a
Erjanazimov <erjan.azimov@gmail.com>
2023-09-17 19:19:31 +0600

    SA-635-Write-tests-on-the-remaining-slices-for-redux-hooks

---
+++ __tests__/hooks/UseWindowWidth.test.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { render, act } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import UseWindowWidth from "../../src/hooks/useWindowWidth";
+jest.mock('../../src/redux/store');
+
+// @ts-ignore
+(global as any).innerWidth = 500;
+const resizeWindow = (x: number) => {
+    // @ts-ignore
+    (global as any).innerWidth = x;
+    window.dispatchEvent(new Event('resize'));
+};
+
+describe('UseWindowWidth', () => {
+    it('returns initial window width', () => {
+        let result: any;
+        function TestComponent() {
+            result = UseWindowWidth();
+            return null;
+        }
+
+        render(<TestComponent />);
+
+        expect(result).toBe(500);
+    });
+
+    it('updates width after window resize', () => {
+        let result: any;
+        function TestComponent() {
+            result = UseWindowWidth();
+            return null;
+        }
+
+        render(<TestComponent />);
+
+        act(() => {
+            resizeWindow(800);
+        });
+
+        expect(result).toBe(800);
+    });
+
+    it('cleans up event listener on unmount', () => {
+        const removeEventListenerSpy = jest.spyOn(window, 'removeEventListener');
+
+        let result: number;
+        function TestComponent() {
+            result = UseWindowWidth();
+            return null;
+        }
+
+        const { unmount } = render(<TestComponent />);
+        unmount();
+
+        expect(removeEventListenerSpy).toHaveBeenCalledWith('resize', expect.any(Function));
+    });
+});
