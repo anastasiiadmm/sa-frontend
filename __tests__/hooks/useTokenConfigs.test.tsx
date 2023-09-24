acf5c0c0e2a73f67123999b26fc645b08ae30ff1
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2023-09-24 22:53:31 +0600

    SA-647 - updated dev dependencies - fixed crush test - updated snapshots

--- __tests__/hooks/useTokenConfigs.test.tsx
+++ __tests__/hooks/useTokenConfigs.test.tsx
@@ -2,30 +2,41 @@ import { renderHook } from '@testing-library/react-hooks';
 import { useSelector } from 'react-redux';
 import "@testing-library/jest-dom";
 import {useTokenConfigs} from "../../src/hooks/useTokensConfigs";
+import store from '../../src/redux/store';
+import { Provider } from "react-redux";
+import React, { ReactNode } from "react";
 jest.mock('../../src/redux/store');
 jest.mock('react-redux', () => ({
     ...jest.requireActual('react-redux'),
     useSelector: jest.fn(),
 }));
 
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+interface WrapperProps {
+    children: ReactNode;
+}
+
+const wrapper: React.FC<WrapperProps> = ({ children }) => <Provider store={store}>{children}</Provider>;
+
 describe('useTokenConfigs', () => {
     it('', () => {
-        // @ts-ignore
-        useSelector.mockImplementation(() => ({
+        (useSelector as jest.Mock).mockImplementation(() => ({
             tokens: {
                 access: 'access_token',
                 refresh: 'refresh_token',
             },
         }));
 
-        const { result } = renderHook(() => useTokenConfigs());
+        const { result } = renderHook(() => useTokenConfigs(), { wrapper });
 
         expect(result.current).toBe("refresh_token");
     });
 
     it('', () => {
-        // @ts-ignore
-        useSelector.mockImplementation(() => ({
+        (useSelector as jest.Mock).mockImplementation(() => ({
             tokens: {
                 access: null,
                 refresh: 'refresh_token',
